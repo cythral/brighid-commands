253fa9894b91c0aa8204355f2f4fe41ed63936b7
Talen Fisher <talen.fisher@cythral.com>
2021-08-10 20:26:15 -0500

    BRGD-168 Embedded Commands Location Logical Grouping

---
+++ src/Service/Database/Migrations/20210811005224_EmbeddedLocationGrouping.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Brighid.Commands.Database
+{
+    public partial class EmbeddedLocationGrouping : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmbeddedLocation",
+                table: "Commands",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.Sql("update Commands set EmbeddedLocation=json_object('AssemblyName', AssemblyName, 'Checksum', Checksum, 'DownloadURL', DownloadURL, 'TypeName', TypeName)");
+
+            migrationBuilder.DropColumn(
+                name: "AssemblyName",
+                table: "Commands");
+
+            migrationBuilder.DropColumn(
+                name: "Checksum",
+                table: "Commands");
+
+            migrationBuilder.DropColumn(
+                name: "DownloadURL",
+                table: "Commands");
+
+            migrationBuilder.DropColumn(
+                name: "TypeName",
+                table: "Commands");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmbeddedLocation",
+                table: "Commands");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TypeName",
+                table: "Commands",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AssemblyName",
+                table: "Commands",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Checksum",
+                table: "Commands",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DownloadURL",
+                table: "Commands",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
