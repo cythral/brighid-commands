e8929a3c2139e30e3ebd39431aab902baaf0a933
Talen Fisher <talen.fisher@cythral.com>
2023-03-27 10:57:37 -0500

    BRGD-275 Recaptcha Authentication (#552)

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 if [ "$Environment" != "local" ]; then
     export Database__Password=$(decrs ${Encrypted__Database__Password}) || exit 1;
-
+    export RecaptchaSettings__SecretKey=$(decrs ${Encrypted__RecaptchaSettings__SecretKey})
     runuser --user brighid /app/Service
     exit $?
 fi
